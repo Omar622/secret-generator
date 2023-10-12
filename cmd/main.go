45b98b4b2f6c9ba589b75767d63dd65c19a43cc0
Omar622 <omaramin622@gmail.com>
2023-10-12 17:31:34 +0300

    Handle reconnection to microsoft access

--- cmd/main.go
+++ cmd/main.go
@@ -8,36 +8,39 @@ import (
 	"github.com/go-ole/go-ole"
 )
 
-func main() {
+func run() {
 	ole.CoInitialize(0)
 	defer ole.CoUninitialize()
 
-	for {
-		fmt.Println("start new session:")
+	fmt.Print("start new session:\n\n")
 
-		sg := app.NewSecretGenerator()
+	sg := app.NewSecretGenerator()
 
-		if err := sg.ReadInput(); err != nil {
-			fmt.Printf("invalid input types: %v\n\n", err)
-			continue
-		}
-
-		if !sg.IsValid() {
-			fmt.Print("invalid input values\n\n")
-			continue
-		}
-
-		w := app.NewWriter(sg.MatchRanges())
-
-		if err := w.WriteReport(); err != nil {
-			fmt.Printf("something went error while writing report: %v\n\n", err)
-			continue
-		}
-
-		if err := w.WriteSecretNumbers(); err != nil {
-			fmt.Printf("something went error while writing secret numbers: %v\n\n", err)
-			continue
-		}
+	if err := sg.ReadInput(); err != nil {
+		fmt.Printf("invalid input types: %v\n\n", err)
+		return
 	}
 
+	if !sg.IsValid() {
+		fmt.Print("invalid input values\n\n")
+		return
+	}
+
+	w := app.NewWriter(sg.MatchRanges())
+
+	if err := w.WriteReport(); err != nil {
+		fmt.Printf("something went error while writing report: %v\n\n", err)
+		return
+	}
+
+	if err := w.WriteSecretNumbers(); err != nil {
+		fmt.Printf("something went error while writing secret numbers: %v\n\n", err)
+		return
+	}
+}
+
+func main() {
+	for {
+		run()
+	}
 }
